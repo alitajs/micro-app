113c47dc00ebd8ba78b1ee3c8a32c9565dca6051
xiaohuoni <448627663@qq.com>
2020-11-06 14:04:37 +0800

    feat: add define config

--- config/config.ts
+++ config/config.ts
@@ -1,10 +1,12 @@
-export default{
-  appType:"h5",
-  mobileLayout:true,
+import { defineConfig } from 'alita';
+
+export default defineConfig({
+  appType: "h5",
+  mobileLayout: true,
   packageId: 'com.alitajs.micro',
   displayIcon: 'src/assets/logo.png',
   displayName: 'micro',
   base: './',
   publicPath: './',
   outputPath: 'platforms/ios/www',
-}
+})
